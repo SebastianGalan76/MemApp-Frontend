ccb15831eb26b45b8c5cd3187411f475064756ec
Sebastian Gałan <sebastian.galan7625@gmail.com>
2025-02-03 18:12:44 +0100

    Added MemeComponent

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MemeContainerComponent } from "./meme-container/meme-container.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MemeContainerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/main/home/meme-container/meme/meme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meme',
+  standalone: true,
+  imports: [],
+  templateUrl: './meme.component.html',
+  styleUrl: './meme.component.scss'
+})
+export class MemeComponent {
+
+}
